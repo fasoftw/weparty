bcff478a95b2d49f5625db7022614d92afc33ce7
Gustavo Alves <alvesvasconcelosg@gmail.com>
2020-11-19 19:21:29 -0300

    Flip Party - Gustavo.

--- src/components/dashboard/PartyItem.vue
+++ src/components/dashboard/PartyItem.vue
@@ -1,40 +1,65 @@
 <template>
     <div class="party-item">
-
-        <div class="party-game-name">
-            <span> Grupo:  <strong>{{ party }}</strong></span>
-        </div>
-
-
-        <div>
-                <ul class="party-players">
-                    <li class="party-player" v-for="player in party.numberPlayers" :key="player">
-                        <div class="party-player-img">
-                            <img 
-                                src="@/assets/desconhecido.jpg"
-                                height="75" width="75" alt="Player"
-                            >
+        
+        <div class="party-inner"   :class="{'flip':isFlipped }">        
+            
+            <div class="flip-card-front">
+                    <div class="party-game-info">
+                        <div class="party-game-name">
+                            <span> Grupo:  <strong>{{ party.name }}</strong></span>
                         </div>
-                        <div class="party-player-info">
+                        <div class="party_icon" v-on:click="flipCard">
+                            <i class="fas fa-info-circle"></i>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+
+
+                    <div class="party_players_button">
+                        <div class="party-players-all">
+                            <ul class="party-players">
+                                <li class="party-player" v-for="player in party.numberPlayers" :key="player">
+                                    <div class="party-player-img">
+                                        <img 
+                                            src="@/assets/desconhecido.jpg"
+                                            height="35" width="35" alt="Player"
+                                        >
+                                    </div>
+                                    <div class="party-player-info">
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="party-buttons">
+                            <b-button class="activated"   @click="enterParty"  v-show="showStatusParty === 'Aberto'"> 
+                                    <i class="fas fa-arrow-alt-circle-right"></i> <span>{{this.showStatusParty}}</span>
+                            </b-button>
+
+                            <b-button class="activated close" v-show="showStatusParty !== 'Aberto'"> 
+                                    <i class="fas fa-lock"></i> <span>{{this.showStatusParty}}</span>
+                            </b-button>
+                        </div>
+
+                    </div>
+            </div>
+
+            <div class="flip-card-back">
+                    <div class="card-back-top">
+                        <div class="card-back-leader">
+                            <span>Lider: <strong>{{party.userId}}</strong></span>
+                        </div>
+                        <div class="party_icon_back" v-on:click="flipCard">
+                            <i class="fas fa-undo-alt"></i>
+                        </div>
+                    </div>
+                    <div class="card-back-description">
+                        <p>Description: {{party.description}}</p>
+                    </div>
+               
+            </div>
+       
         </div>
 
-
-        <div class="party-buttons">
-             <b-button class="activated"   @click="enterParty"  v-show="showStatusParty === 'Aberto'"> 
-                    <i class="fas fa-arrow-alt-circle-right"></i> <span>{{this.showStatusParty}}</span>
-            </b-button>
-
-            <b-button class="activated close" v-show="showStatusParty !== 'Aberto'"> 
-                    <i class="fas fa-lock"></i> <span>{{this.showStatusParty}}</span>
-            </b-button>
-        </div>
-
-        <div class="party-status">
-            <span>{{this.qtdPlayers}}/{{party.numberPlayers}}</span>
-        </div>
     </div>
 </template>
 
@@ -51,6 +76,7 @@ export default {
             statusParty: null,
             showStatusParty: 'Aberto',
             qtdPlayers: null,
+            isFlipped: true,
         }
     },
     methods:{
@@ -58,14 +84,18 @@ export default {
 
         },
         enterParty(){
-
+            this.statusParty = !this.statusParty
+            console.log(this.statusParty)
         },
         countPlayers(){
-            console.log(this.party.gameId)
+           // console.log(this.party.gameId)
             axios.get(`${baseApiUrl}/gamesParties/${this.party.gameId}`).then((res) => {
                 this.players = res.data
-                console.log(this.players)
+              //  console.log(this.players)
             })
+        },
+        flipCard(){
+            this.isFlipped = !this.isFlipped 
         }
     },
     watch: {
@@ -84,26 +114,73 @@ export default {
 <style>
 
 .party-item{
+    display: flex;
+    width: 380px;
+    height: 270px;
+    background-color: transparent;
+    perspective: 1000px;
     border-radius: 25px;
     margin-bottom: 15px;
-    background-color: #f2f2f2;
+    background-color: #ffffff;
     padding: 0px 20px 20px;
     border: 1px solid rgba(0, 0, 0, 0.2);
     box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
 }
 
+.party-inner{
+    display:flex;
+    flex-direction: column;
+    flex-grow: 1; 
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+}
 
-.party-game-name{
+.flip {
+  transform: rotateY(180deg) ;
+  
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
     display: flex;
-    justify-content: center;
-    color:black;
-    border-width: 5px;
+    flex-direction: column;
+    flex-grow: 1;
+}
+
+.flip-card-back {    
+    display: flex;
+    flex-grow: 1;
+    transform: rotateY(180deg);
+}
+
+
+.party-back{
+  padding: 18px 0;
+}
+
+.party-game-info{
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    justify-content: space-around;    
+    border-width: 10px;
     border-style: solid;
     border-image: 
         linear-gradient( 
         #6666ff,
         rgba(0, 0, 0, 0)
         ) 0 0 90%;
+}
+
+.party-game-name{
+    flex-grow: 1;
 }
 
 .party-game-name span{
@@ -116,36 +193,36 @@ export default {
 }
 
 
+.party-players-all{
+    flex-grow: 1;
+}
 .party-players{
-    flex: 1;
     display: flex;
+    flex-grow: 1;
     flex-direction: row;
-    justify-content: space-around;
+    justify-content: flex-start !important;
     flex-wrap: wrap;
-    width: 100%;
     padding-left: 0;
 }
 
 .party-player{
-    
     margin: 5px;
-    margin-bottom: 20px;    
     background-color: #f2f2f2;
     padding: 20px;
     border: 1px solid rgba(0, 0, 0, 0.2);
-    border-radius: 10px;
+    border-radius: 50%;
 }
 
 
 
-
-
+.party_players_button{
+    flex-grow: 1;
+    display:flex;
+    flex-direction: column;
+}
 
 .party-buttons{
-    flex: 1;
     display: flex;
-    flex-direction: row;
-    justify-content: center;
 }
 
 .party-buttons button{
@@ -184,7 +261,7 @@ export default {
 }
 
 .close:hover{
-    cursor: not-allowed !important;
+    cursor: not-allowed ;
     opacity: .7;
     background-color: #af2424 !important;
     color: black !important;
@@ -192,6 +269,40 @@ export default {
 
 
 
+.flip-card-back{
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+}
+.card-back-top{
+     display: flex;
+    flex-direction: row;
+    text-align: center;   
+    border-width: 10px;
+    border-style: solid;
+    border-image: 
+        linear-gradient( 
+        #6666ff,
+        rgba(0, 0, 0, 0)
+        ) 0 0 90%;
+}
+.card-back-leader{
+    flex-grow: 1    ;
+}
+
+.card-back-leader span{
+    font-family:sans-serif;
+    font-weight: 400;
+}
+.card-back-leader strong{
+    font-family:sans-serif;
+    font-weight: 800;
+}
+
+.card-back-description{
+    margin-left: 10px;
+    flex-grow: 1;
+}
 
 
         
